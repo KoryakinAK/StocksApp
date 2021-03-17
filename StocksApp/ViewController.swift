e1de35d6ea4229d5a58aa411e2ccaa1f4a8dd40e
Koryakin Alexey <un@Alexeys-MacBook-Pro.local>
2021-03-17 18:56:29 +0300

    Создан пустой проект

---
+++ StocksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Alexey Koryakin on 17.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+

fdf70508a99057ff9f3d8a27ebe902cd96a7580d
Koryakin Alexey <un@Alexeys-MacBook-Pro.local>
2021-04-04 14:29:08 +0300

    Создан UDManager для управления статусом избранных акций

---
+++ StocksApp/Workers/UserDefaultsManager/UserDefaultsManager.swift
@@ -0,0 +1,48 @@
+//
+//  UserDefaultsManager.swift
+//  StocksApp
+//
+//  Created by Alexey Koryakin on 03.04.2021.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    private static let instance = UserDefaultsManager()
+    private let favsKey = "Favourites"
+
+    class func sharedInstance() -> UserDefaultsManager {
+        return instance
+    }
+
+    private func getFavouritesArray() -> [String] {
+        guard
+            let favouritesAsAny = UserDefaults.standard.value(forKey: favsKey),
+            let favourites = favouritesAsAny as? [String]
+        else {
+            return [String]()
+        }
+        return favourites
+    }
+
+    func addToFavourites(ticker: String) {
+        print("adding \(ticker)")
+        var favourites = getFavouritesArray()
+        if !favourites.contains(ticker) {
+            favourites.append(ticker)
+        }
+        UserDefaults.standard.setValue(favourites, forKey: favsKey)
+    }
+
+    func removeFromFavourites(ticker: String) {
+        var favourites = getFavouritesArray()
+        print("removing \(ticker)")
+        favourites = favourites.filter {$0 != ticker}
+        UserDefaults.standard.setValue(favourites, forKey: favsKey)
+    }
+
+    func checkIfFavouritesContain(ticker: String) -> Bool {
+        let favourites = getFavouritesArray()
+        return favourites.contains(ticker)
+    }
+}
